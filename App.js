85e8ab4d31c7e951a9ff86befd82709580af1e6e
harish <harisht1320@gmail.com>
2025-09-28 10:15:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import SendScreen from "./src/screens/SendScreen";
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Demo Wallet</Text>
-      <Text>Build successful!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Wallet" component={HomeScreen} />
+        <Stack.Screen name="Send" component={SendScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
